7af3b2105e371c92062f89289cf260313b1d48e1
Saturday <zhangkairong123@qq.com>
2025-02-24 10:47:27 +0800

    build:  调整main 打包产物入口💡

--- build-mate.config.ts
+++ build-mate.config.ts
@@ -1,21 +1,10 @@
+import { defineConfig } from '@coveyz/build-mate';
 
-
-// console.log('xxx');
-
-const defineConfig = (options: any) => {
-    return options;
-};
-
-
-export default defineConfig([
-    {
-        name: 'build-mate',
-        entry: ['src/index.ts'],
-        target: 'node16.14',
-        dts: {
-            resolve: true,
-            entry: './src/index.ts',
-        },
-        outDir: "buildMate"
+export default defineConfig({
+    name: 'build-mate',
+    target: 'node16.14',
+    dts: {
+        resolve: true,
+        entry: './src/index.ts',
     },
-]);
+});
